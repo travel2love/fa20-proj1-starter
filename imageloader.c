22bfcf1e174f99ae3da4af4c6767244046050f27
travel2love <irelia2@qq.com>
2024-05-26 21:58:08 +0800

    with

--- imageloader.c
+++ imageloader.c
@@ -26,16 +26,35 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+	FILE *fp = fopen(filename, "r");
+	char name[20];
+	int cols = 0, rows = 0, number = 0;
+	fscanf(fp, "%s\n%d %d\n%d", name, &cols, &rows, &number);
+	Image *images = (Image *)malloc(sizeof(Color*)*rows*cols);
+	images->rows = rows;
+	images->cols = cols;
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			Color *colorptr, color;
+			colorptr = &color;
+			fscanf(fp, "%hhd %hhd %hhd", &color.R, &color.G, &color.B);
+			images->image = &color;
+		}
+	}
+	return images;
+	fclose(fp);
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+		
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+	free(image);
 }
