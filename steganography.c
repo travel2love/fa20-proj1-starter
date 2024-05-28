95f6eae2467f01a68127c8de00f24ae41b0cab6e
travel2love <irelia2@qq.com>
2024-05-28 17:12:02 +0800

    with

--- steganography.c
+++ steganography.c
@@ -22,12 +22,50 @@
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	Color *color = (Color*)malloc(sizeof(Color));
+	Image *images = image;
+	Color **images_0 = images->image;
+	Color *images_1 = *images_0;
+	int B = 0, image_col = image->cols;
+	int k = image_col*row + col;
+	B = (images_1+k)->B;
+	if(B << 7 == 0) {
+		color->R = 0;
+		color->G = 0;
+		color->B = 0;
+	} else {
+		color->R = 255;
+		color->G = 255;
+		color->B = 255;
+	}
+	return color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	Image *images = image;
+	int rows = images->rows;
+	int cols = images->cols;
+	Image *new_image = (Image *)malloc(sizeof(Color *)*rows*cols);
+	new_image->rows = rows;
+	new_image->cols = cols;
+	Color **color_star;
+	new_image->image = color_star;
+	Color *new_colorptr[cols*rows], new_color[cols*rows];
+       	int k = 0;	
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < cols; j++) {
+			Color *color = evaluateOnePixel(images, i, j);
+			color_star = &color;
+//			*color_star += 1;
+			new_color[k] = *color;
+			new_colorptr[k] = &new_color[k];
+			k += 1;
+		}
+	}
+	return new_image;
 }
 
 /*
@@ -46,4 +84,15 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	FILE *fp = fopen(argv[1], "r");
+	if(fp == NULL) {
+		return -1;
+	}
+	Image *image = readData(argv[1]);
+	Image *image_new = steganography(image);
+	writeData(image_new);
+	freeImage(image);
+	freeImage(image_new);
+	return 0;
+	
 }
